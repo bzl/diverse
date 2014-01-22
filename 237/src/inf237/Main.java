da2393c0e1e5ff7281fce47064b079718f91193a
bzl <Bluekristoffer@gmail.com>
2014-01-22 12:13:40 +0100

    sample input fil, og ferdig Main metode med gyldig svar på 3721

--- 237/src/inf237/Main.java
+++ 237/src/inf237/Main.java
@@ -1,8 +1,20 @@
 package inf237;
 
+import java.text.DecimalFormat;
+
 public class Main {
 
 	public static void main(String[] args){
-		System.out.println("test");
+
+		double test = 1.9230769230769231;
+		double test2 = 1.56262341;
+
+		DecimalFormat df = new DecimalFormat("#.00");
+
+	    
+	    
+		System.out.printf("%.3f %.3f", test, test2);
+		
+		// System.out.println(System.getProperty("java.version"));
 	}
 }
