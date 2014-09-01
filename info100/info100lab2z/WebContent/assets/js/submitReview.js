d55b4db0b0dc89e9d34c6e49e5e2da7f96c4f587
ktv095 <bluekristoffer@gmail.com>
2014-09-01 21:47:43 +0200

    posting til database implementert.

--- info100/info100lab2z/WebContent/assets/js/submitReview.js
+++ info100/info100lab2z/WebContent/assets/js/submitReview.js
@@ -1,17 +1,17 @@
 function submitReview(){
 
-    
-    var valgtStudie = $("#second-choice").val();
-                var valgtSkole = $("#first-choice").val();
-                
-                var reviewText = $("#evaluering").val();
-                var reviewAuthor = $("#evaluer_navn").val();
-                var review = "\"" + reviewText + "\" - " + reviewAuthor;
-    
-    var url = encodeURI("assets/php/submitReview.php?skole=" + valgtSkole + "&studie=" + valgtStudie + "&review" + review);
-                    var content;
-                    $.get(url, function(data) {
-                        content = data;
-                        $('#evalueringer').prepend(content);
-                    });
+	var valgtStudie = $("#second-choice").val();
+	var valgtSkole = $("#first-choice").val();
+
+	var reviewText = $("#evaluering").val();
+	var reviewAuthor = $("#evaluer_navn").val();
+	var review = "\"" + reviewText + "\" - " + reviewAuthor;
+
+	var url = encodeURI("assets/php/leggTilEvaluering.php?skole=" + valgtSkole + "&studie=" + valgtStudie + "&review=" + review);
+	var content;
+	$.get(url, function(data) {
+		content = data;
+		$('#evalueringer').prepend(content);
+	});
+	window.location = "kontakt.html";
 }
